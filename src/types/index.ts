6d8cdc5d705129a1441494db75c2645e497091b1
iMac 27 <imac27@iMac-Crashe24.local>
2024-06-28 15:48:29 -0500

    change with valibot

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,4 +1,4 @@
-import { object, string, number, boolean, Output, array } from 'valibot';
+import { object, string, number, boolean, InferOutput, array } from 'valibot';
 
 export const DraftProductSchema = object({
     name: string(),
@@ -13,4 +13,4 @@ export const ProductSchema = object({
 })
 
 export const ProductsSchema = array(ProductSchema)
-export type Product = Output<typeof ProductSchema>
+export type Product = InferOutput<typeof ProductSchema>
